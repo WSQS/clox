8bc9888b2b71cfba4d4ce9babee54e0a5e8bdd33
sophomore <sophomore@duck.com>
2025-08-21 13:28:19 +0800

    finish chapter 18

--- common.h
+++ common.h
@@ -1,10 +1,12 @@
 #pragma once
+#include <stdarg.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 
 #define DEBUG_PRINT_CODE
 #define DEBUG_TRACE_EXECUTION
