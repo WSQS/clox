408904b01b8ce2178d6cc16c00f3c1cf54a053d2
sophomore <sophomore@duck.com>
2025-08-25 16:48:54 +0800

    finish chapter 20

--- vm.c
+++ vm.c
@@ -29,8 +29,10 @@ static void runtimeError(const char *format, ...) {
 void initVM() {
     resetStack();
     vm.objects = NULL;
+    initTable(&vm.strings);
 }
 void freeVM() {
+    freeTable(&vm.strings);
     freeObjects();
 }
 
