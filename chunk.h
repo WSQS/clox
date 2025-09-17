902e60dd40420886791fe69d70ca8047b08e02d9
sophomore <sophomore@duck.com>
2025-09-17 17:41:07 +0800

    finish chapter 28

--- chunk.h
+++ chunk.h
@@ -32,10 +32,12 @@ typedef enum {
     OP_LOOP,
     OP_PRINT,
     OP_CALL,
+    OP_INVOKE,
     OP_CLOSURE,
     OP_CLOSE_UPVALUE,
     OP_RETURN,
     OP_CLASS,
+    OP_METHOD
 } OpCode;
 
 typedef struct {
